c6f8ef4b4a8b4b4dc3d2ffd74c32c7b1af776c21
Alessio <lessju@gmail.com>
2014-03-07 15:02:31 +0100

    Added beamforming pipeline document

--- src/beamformer/beamforming_kernel.cu
+++ src/beamformer/beamforming_kernel.cu
@@ -1,13 +1,7 @@
 #ifndef BEAMFORMING_KERNEL_H_
 #define BEAMFORMING_KERNEL_H_
 
-#define PFB_THREADS 128
-#define BEAMFORMER_THREADS 128
-#define BEAMS_PER_TB 8
-#define BEAMS 8
-#define ANTS 32
-#define HEAP 128 // Specific to Medicina data output format
-#define NTAPS 32  // Number of taps for PFB FIR
+#include "params.h"
 
 // --------------------------------------- Beamforming Kernels --------------------------------------
 
---
+++ src/beamformer/params.h
@@ -0,0 +1,7 @@
+#define PFB_THREADS 128
+#define BEAMFORMER_THREADS 128
+#define BEAMS_PER_TB 8
+#define BEAMS 8
+#define ANTS 32
+#define HEAP 128  // Specific to Medicina data output format
+#define NTAPS 32  // Number of taps for PFB FIR
